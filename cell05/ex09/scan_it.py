b4d1a7ac08e958a37160bc87d91a1e823b59fc43
0310_Natnaree Moeitom <moeitomnatnaree@gmail.com>
2026-02-12 01:39:10 +0700

    upload

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -14,3 +14,23 @@ else :
         print("none")
     else:
         print(count)
+
+
+
+#!/usr/bin/env python3
+# import sys
+# import re
+
+# if len(sys.argv) != 3:
+#     print("none")
+# else:
+#     keyword = sys.argv[1]
+#     text = sys.argv[2]
+
+#     matches = re.findall(re.escape(keyword), text)  # escape กัน keyword เป็น regex พิเศษ
+#     count = len(matches)
+
+#     if count == 0:
+#         print("none")
+#     else:
+#         print(count)
